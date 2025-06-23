59ff93d0b10131cc418ed5d69b7fec16c27c2afd
Román Abalos <romanishida2602@gmail.com>
2025-06-22 21:13:18 -0300

    Update Main.java

--- tp_files/src/main/java/tp_files/Main.java
+++ tp_files/src/main/java/tp_files/Main.java
@@ -8,11 +8,11 @@ public class Main {
         EntradaDatos entrada = new EntradaDatos();
         int[] vector = entrada.IngresarDatosVector();
 
-        // Guardar vector en archivo
+
         ManejadorArchivo archivo = new ManejadorArchivo("datos.txt");
         archivo.crearFileConBuffer(archivo.getFiles(), vector);
 
-        // Procesar divisiones y generar archivos de salida
+
         Procesos proc = new Procesos();
         proc.procesarVector(vector);
     }
