e619b70f78d5729e75f3745d49d4d578323d3987
romaishi <romanishida2602@gmail.com>
2025-09-03 12:40:10 -0300

    Update MonitorThread.java

--- SocketHilos/src/main/java/Dowload/MonitorThread.java
+++ SocketHilos/src/main/java/Dowload/MonitorThread.java
@@ -1,5 +1,42 @@
 package Dowload;
 
-public class MonitorThread {
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+public class MonitorThread extends Thread{
+
+	private ThreadGroup grupo;
+	private ArrayList<Thread> hilos;
+	private static PrintStream ps = new PrintStream(System.out);
+	
+	public MonitorThread(ThreadGroup group, ArrayList<Thread> lista, Map<String,String> coloresHilos) {
+		super("Monitor-Hilo");
+		this.currentThread().setPriority(10);
+		this.hilos = lista;
+		this.grupo = group;
+		this.setDaemon(true);
+		
+	}
+	
+	@Override
+	public void run() {
+		Map<String, Boolean> completados = new HashMap<>();
+		
+		for()
+		
+		boolean activos = true;
+		while(activos)
+		{
+			for()
+			{
+				
+			}
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) 
+		}
+	}
+	
 }
